607401f5c4c0a3a5867925f54ce2d7e20a6c03bd
ARH80 <hamzei@ce.sharif.edu>
2020-07-15 11:29:38 +0430

    Server beginning

--- src/main/java/network/Server.java
+++ src/main/java/network/Server.java
@@ -1,4 +1,46 @@
 package network;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    public static void main(String[] args) {
+        new ServerHelp().runServer();
+    }
+
+    private static class ServerHelp {
+        private void runServer() {
+            try {
+                ServerSocket serverSocket = new ServerSocket(8888);
+                while (true) {
+                    Socket clientSocket;
+                    clientSocket = serverSocket.accept();
+                    System.out.println("New Client Accepted");
+                    DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+                    DataOutputStream dataOutputStream = new DataOutputStream(new BufferedOutputStream(clientSocket.getOutputStream()));
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class ClientHandler extends Thread {
+        private InputStream inputStream;
+        private OutputStream outputStream;
+        private ServerHelp serverHelp;
+
+        public ClientHandler(InputStream inputStream, OutputStream outputStream, ServerHelp serverHelp) {
+            this.inputStream = inputStream;
+            this.outputStream = outputStream;
+            this.serverHelp = serverHelp;
+        }
+
+        private void handleClient() {
+
+        }
+    }
 }
